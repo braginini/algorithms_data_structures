d23235d1359b17f0cefa3c5fdafa8a824946a8c2
mikhail.bragin <mikhail@grupoxago.com>
2013-11-29 18:15:25 -0200

    fix for bst

--- src/main/java/com/test/algorithms/datastructure/RedBlackBST.java
+++ src/main/java/com/test/algorithms/datastructure/RedBlackBST.java
@@ -118,6 +118,59 @@ public class RedBlackBST<K extends Comparable<K>, V> {
 
     }
 
+	public void delete(K key) {
+
+		root = delete(root, key);
+	}
+
+	private Node delete(Node x, K key) {
+
+		if (x == null)
+			return null;
+
+		int cmp = key.compareTo((K) x.key);
+
+		if (cmp < 0)
+			x.left = delete(x.left, key);
+		else if (cmp > 0)
+			x.right = delete(x.right, key);
+		else {
+			if (x.right == null)
+				return x.left;
+
+			Node t = x;
+
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+
+		return x;
+	}
+
+	private Node min(Node root) {
+
+		Node x = root;
+
+		if (x == null)
+			return null;
+
+		while (x.left != null) {
+			x = x.left;
+		}
+
+		return x;
+	}
+
+	private Node deleteMin(Node x) {
+		if (x.left == null)
+			return x.right;
+
+		x.left = deleteMin(x.left);
+
+		return x;
+	}
+
     private boolean isRed(Node x) {
         if (x == null)
             return false;
